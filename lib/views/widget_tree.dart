1f67f17d6a667ab8b436382685d6b694f71fc0d9
Hasan AbouMinyar <among.00.12@gmail.com>
2025-05-09 23:17:09 +0200

    set actions

--- lib/views/widget_tree.dart
+++ lib/views/widget_tree.dart
@@ -1,23 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/data/notifiers.dart';
 import 'package:flutter_app/views/pages/home_page.dart';
 import 'package:flutter_app/views/pages/profile_page.dart';
 import 'package:flutter_app/views/widgets/navbar_widget.dart';
 
-
-List <Widget> pages = [
-  HomePage(),
-  ProfilePage(),
-];
+List<Widget> pages = [HomePage(), ProfilePage()];
 
 class WidgetTree extends StatelessWidget {
-  const WidgetTree ({super.key});
+  const WidgetTree({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: Text('Diglobtal Shop')),
-        bottomNavigationBar: NavbarWidget(),
-        body: pages.elementAt(1),
-      );
+      appBar: AppBar(title: Text('Diglobtal Shop')),
+      bottomNavigationBar: NavbarWidget(),
+      body: ValueListenableBuilder(
+        valueListenable: selectedPageNotifier,
+        builder: (context, selectedPage, child) {
+          return pages.elementAt(selectedPage);
+        },
+      ),
+    );
   }
 }
