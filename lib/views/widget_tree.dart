fa1899aaac294887c7f45c09640e1e2c1bb0461c
Hasan AbouMinyar <among.00.12@gmail.com>
2025-05-10 01:21:57 +0200

    add dark and ligth mode

--- lib/views/widget_tree.dart
+++ lib/views/widget_tree.dart
@@ -12,7 +12,22 @@ class WidgetTree extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Diglobtal Shop')),
+      appBar: AppBar(
+        title: Text('Diglobtal Shop'),
+        actions: [
+          IconButton(
+            onPressed: () {
+              isDarkModeNotifier.value = !isDarkModeNotifier.value;
+            },
+            icon: ValueListenableBuilder(
+              valueListenable: isDarkModeNotifier,
+              builder: (context, isDarkMode, child) {
+                return Icon(isDarkMode ? Icons.light_mode : Icons.dark_mode,);
+              },
+            ),
+          ),
+        ],
+      ),
       bottomNavigationBar: NavbarWidget(),
       body: ValueListenableBuilder(
         valueListenable: selectedPageNotifier,
